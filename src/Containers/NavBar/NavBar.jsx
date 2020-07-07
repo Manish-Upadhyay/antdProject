b148b906f1eafbc4f5c3637c87219d19457449a4
Manish-Upadhyay <manish.upadhyay2017@outlook.com>
2020-07-07 20:56:07 +0530

    hello

--- src/Containers/NavBar/NavBar.jsx
+++ src/Containers/NavBar/NavBar.jsx
@@ -11,9 +11,9 @@ const NavBar = () => {
         defaultSelectedKeys={["2"]}
         style={{ lineHeight: "40px" }}
       >
-        <Menu.Item key="1">nav 1</Menu.Item>
-        <Menu.Item key="2">nav 2</Menu.Item>
-        <Menu.Item key="3">nav 3</Menu.Item>
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2">Courses</Menu.Item>
+        <Menu.Item key="3">Tutorials</Menu.Item>
       </Menu>
     </Layout.Header>
   );
