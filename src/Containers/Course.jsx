5e3e525fa1c9cc7e64e651dedb98bb44e35e4730
Manish-Upadhyay <manish.upadhyay2017@outlook.com>
2020-07-12 20:44:36 +0530

    changes

---
+++ src/Containers/Course.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Course extends Component {
+  render() {
+    return <div>Course</div>;
+  }
+}
+
+export default Course;
---
+++ src/Containers/Tutorial.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Tutorila extends Component {
+  render() {
+    return <div>sdsd</div>;
+  }
+}
